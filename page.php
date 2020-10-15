18a47272bdd4d2341b2ed33a7cd2f5ac6210b835
Sergei827 <marchukcha89@gmail.com>
2020-10-15 23:22:20 +0300

    first commit
    Простая структура сайта

---
+++ page.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset='utf-8'>
+    <link href='css/style.css' rel='stylesheet'>
+    <title>NIX lessons</title>
+</head>
+<body>
+<header>
+    <div class='header-wrapper'>
+	   <span class='title-nix'>NIX</span> 
+	   <span class='title-lessons'>lessons</span>
+	</div>
+</header>
+<nav>
+    <ul class='menu'>
+	    <li class='menu-item'>
+		    <a href='#' class='menu-link'>Главная</a>
+		</li>
+	    <li class='menu-item'>
+		    <a href='#' class='menu-link'>Урок 1</a>
+		</li>
+		<li class='menu-item'>
+		    <a href='#' class='menu-link'>Урок 2</a>
+		</li>
+	</ul>
+</nav>
+<main>
+    <div class='main-wrapper'>
+	    <p>Основное содердимое</p>
+	</div>
+</main>
+<footer>
+    <div class='footer-wrapper'>
+	    <p class='footer-copy'>&copy; Сергей Марченко, 2020</p>
+	</div>
+</footer>
+</body>
+</html>
